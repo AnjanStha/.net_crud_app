8c5f23a84ae7ecfd60f33b485c19fee14726c01d
AnjanStha <anjanstha61@gmail.com>
2024-02-01 10:19:55 +0545

    .net core mvc crud app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using studentWebPage.Models;
+using System.Diagnostics;
+
+namespace studentWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentWebPage.Models.Entities;
+
+namespace studentWebPage.data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+  
+}
